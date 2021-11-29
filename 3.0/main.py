84494ecd9d3a43018729204146d7d5bdb9614d28
Ashraf-wan <hackeranimate@gmail.com>
2021-11-29 13:49:27 +0000

    3.0

--- 3.0/main.py
+++ 3.0/main.py
@@ -1,12 +1,39 @@
-from flask import Flask, render_template
+import os
 
-app = Flask(__name__, template_folder='template')
+os.system("clear")
 
-@app.route("/")
-def index():
-    return render_template('index.html')
-@app.route("/login")
-def login():
-    return render_template('login.html')
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8080)
+print("""
+
+                 __          
+ /  _ _ _   _/  / _)' __/_   
+(__(-(/(-/)(/  /(_)/_) // () 
+    _/                       
+
+Welcome To Legend Bistro
+
+[1]List the menu  [3]Admin panel
+[2]Order          [4]Employee login
+""")
+
+lb = input("What do you want to do?\n")
+
+if lb == "1":
+    print(
+        "pepperoni pizza,home special,cheese pizza,ham and cheese pizza,sausage pizza"
+    )
+elif lb == "2":
+    os.system("python3 order.py")
+elif lb == "3":
+    code = input("Please put the code. ")
+    if code == "1021":
+        os.system("python3 admin.py")
+    else:
+        print("Sorry,wrong code.")
+elif lb == "4":
+    code1 = input("Please put the code. ")
+    if code1 == "2021":
+        os.system("python3 employee.py")
+elif "refund" in lb:
+    print("Here is you refund.")
+else:
+    print("invalid options")
