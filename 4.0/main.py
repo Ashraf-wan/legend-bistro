862ca808c434c751b3c82910b6c3d07b74df52b6
Wan Ashraf <73482535+Ashraf-wan@users.noreply.github.com>
2021-12-10 22:43:54 +0800

    stuff

--- 4.0/main.py
+++ 4.0/main.py
@@ -5,8 +5,8 @@ app = Flask(__name__, template_folder='template')
 @app.route("/")
 def index():
     return render_template('index.html')
-@app.route("/login")
-def login():
-    return render_template('login.html')
+@app.route("Admin")
+def admin():
+    return render_template('admin.html')
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=8080)
--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM debian
 RUN apt update && apt dist-upgrade -y
 RUN apt install python3 python3-pip git -y
-RUN pip3 install flask
 RUN git clone https://github.com/Ashraf-wan/legend-bistro
+WORKDIR "legend-bistro"
+RUN pip install -r requirements.txt
 WORKDIR "legend-bistro/4.0"
 EXPOSE "8080"
-RUN python3 main.py
